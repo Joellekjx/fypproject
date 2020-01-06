aa32c82f1f3346972eca974c641d90885aa50cf8
joellekjx <joellekjx@gmail.com>
2020-01-07 00:56:33 +0800

    merge

--- trial-react-big-calendar/src/App.js
+++ trial-react-big-calendar/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import MainCalendar from './components/mainCalendar';
 import LeftSideColumn from './components/leftSideColumn';
 import { Grid } from '@material-ui/core';
+import { observer } from "mobx-react";
 
 class App extends Component {
   render() {
+    const { calendarStore } = this.props;
     return (
       <div className="App">
         <Grid container>
@@ -12,7 +14,7 @@ class App extends Component {
             <LeftSideColumn />
           </Grid>
           <Grid item xs={10}>
-            <MainCalendar />
+            <MainCalendar calendarStore={calendarStore} />
           </Grid>
         </Grid>
       </div>
@@ -20,4 +22,5 @@ class App extends Component {
   }
 }
 
+App = observer(App);
 export default App;
--- trial-react-big-calendar/src/index.js
+++ trial-react-big-calendar/src/index.js
@@ -5,18 +5,45 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import WeeklyReport from './components/weeklyReport';
 import Meeting from './components/meeting';
-
+import CalendarStore from './mobx-store/CalendarStore';
 import * as serviceWorker from './serviceWorker';
+
+const calendarStore = new CalendarStore();
+
+const PassPropsToApp = () => {
+    return(
+        <App
+            calendarStore={calendarStore}
+        />
+    )
+}
+
+const PassPropsToWR = () => {
+    return(
+        <WeeklyReport
+            calendarStore={calendarStore}
+        />
+    )
+}
+
+const PassPropsToMeeting = () => {
+    return(
+        <Meeting
+            calendarStore={calendarStore}
+        />
+    )
+}
 
 const routing = (
     <Router>
         <div>
-            <Route exact path="/" component={App} />
-            <Route path="/weeklyreport" component={WeeklyReport} />
-            <Route path="/meetings" component={Meeting} />
+            <Route exact path="/" component={PassPropsToApp}/>
+            <Route path="/weeklyreport" component={PassPropsToWR} />
+            <Route path="/meetings" component={PassPropsToMeeting} />
         </div>
     </Router>
 )
+
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
