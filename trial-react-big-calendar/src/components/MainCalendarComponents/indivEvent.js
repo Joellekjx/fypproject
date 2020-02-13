a89e88b4042703d3740b9e87b2366aa3046cd921
Okseeyou <celiaongsl@gmail.com>
2020-02-13 19:55:40 +0800

    adjusted popup paper to look a lil better

--- trial-react-big-calendar/src/components/MainCalendarComponents/indivEvent.js
+++ trial-react-big-calendar/src/components/MainCalendarComponents/indivEvent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Paper, Popover, Button } from '@material-ui/core';
+import { Paper, Popover, Button, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 import history from '../../history';
@@ -10,11 +10,18 @@ const useStyles = makeStyles(theme => ({
       display: 'flex',
       flexWrap: 'wrap',
       '& > *': {
-      //   margin: theme.spacing(1),
-        width: theme.spacing(16),
+        minWidth: '300px',
         height: theme.spacing(16),
-        padding: theme.spacing(5),
+        padding: theme.spacing(3),
       },
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(20),
+      fontWeight: 'bold'
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
     },
   }));
 
@@ -24,7 +31,6 @@ export default function CustomEventWithPopover(props){
       const [anchorEl, setAnchorEl] = React.useState(null);
   
       const handleClick = e => { //i think smth is wrong here...
-        // console.log(event);
         setAnchorEl(e.currentTarget);
       };
     
@@ -51,24 +57,52 @@ export default function CustomEventWithPopover(props){
             return "Nothing";
         }
       }
+
+      const formatDateView = () => {
+        //this is so that if meetings ever cross days (e.g. 12 March 2020 2pm - 13 March 2020 3pm for what ever reason)
+        //we can set it properly
+        var eventStart = event.event.start;
+        var eventEnd = event.event.end;
+        console.log(event.title);
+        var eventStartFormat = moment(eventStart).format('dddd, MMMM DD YYYY')
+        switch(event.title){
+          case "Meeting Notes":
+            return(
+              <React.Fragment>
+                {eventStartFormat} &bull; {`${moment(event.event.start).format('HH:mm')}-${moment(event.event.end).format('HH:mm')} `}
+              </React.Fragment>
+            )
+          default:
+            return(
+              <React.Fragment>
+                {eventStartFormat}
+              </React.Fragment>
+            )
+        }
+      }
   
+      // Other things you can add: Meeting's description (mainly location), status of submission, whether there's attachments
       const renderPopoverPaper = () => {
-        var eventStart = event.start;
+        var eventStart = event.event.start;
+        var eventEnd = event.event.end;
         return(
           <div className={classes.root}>
-              <Paper>
-                  can i use a paper<br/>
-                  {event.title}
-                  {event.content}
-                  {event.Id}
-                  {moment(eventStart).format('lll')}
-                  <Button>
-                      {/* This should do an axios delete */}
-                      Delete
-                  </Button>
-                  <Button variant="contained" color="primary" onClick={onRoutingButtonClick}>
-                      More Info
-                  </Button>
+              <Paper style={{position: 'relative'}}>
+                  <Typography className={classes.heading}>{event.title}</Typography>
+                  {/* {event.title} */}
+                  {/* <Typography className={classes.secondaryHeading}>{event.event.content}</Typography> */}
+                  <Typography className={classes.secondaryHeading}>
+                    {formatDateView()}
+                  </Typography>
+                  <div style={{position: 'absolute', bottom: '0', float: 'right', paddingBottom: '15px'}}>
+                    <Button style={{marginRight: '15px'}}>
+                        {/* This should do an axios delete */}
+                        Delete
+                    </Button>
+                    <Button variant="contained" color="primary" onClick={onRoutingButtonClick}>
+                        More Info
+                    </Button>
+                  </div>
               </Paper>
           </div>
         )
