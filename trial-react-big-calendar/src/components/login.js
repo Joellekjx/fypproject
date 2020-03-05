18c5af22da15ae83b4a33bca6315d5db8f5624b7
joellekjx <joellekjx@gmail.com>
2020-03-05 22:46:05 +0800

    update login form

--- trial-react-big-calendar/src/components/login.js
+++ trial-react-big-calendar/src/components/login.js
@@ -11,6 +11,7 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { withStyles } from '@material-ui/core/styles';
 
@@ -48,6 +49,13 @@ const useStyles = (theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  root: {
+    display: 'flex',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+    alignItems: 'center',
+  },
 })
 
 class Login extends Component {
@@ -81,75 +89,100 @@ class Login extends Component {
     }
 
     render() {
-        const { classes } = this.props;
-        return (
-            <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar} style={{margin: 'auto'}}>
-                <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                Sign in
-                </Typography>
-                <form className={classes.form} noValidate onSubmit={this.handleLogin} method="POST">
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="username"
-                    label="Username"
-                    name="username"
-                    autoComplete="username"
-                    onChange={this.handleChange}
-                    autoFocus
-                />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    onChange={this.handleChange}
-                />
-                <FormControlLabel
-                    control={<Checkbox value="remember" color="primary" />}
-                    label="Remember me"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className={classes.submit}
-                >
-                    Sign In
-                </Button>
-                {/* <Grid container>
-                    <Grid item xs>
-                    <Link href="#" variant="body2">
-                        Forgot password?
-                    </Link>
-                    </Grid>
-                    <Grid item>
-                    <Link href="#" variant="body2">
-                        {"Don't have an account? Sign Up"}
-                    </Link>
-                    </Grid>
-                </Grid> */}
-                </form>
-            </div>
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-            </Container>
-        );
-    }
+      let errorMessage = null;
+      if (this.props.error) {
+          errorMessage = (
+          <p>{this.props.error.message}</p>
+          );
+      }
+
+      const { username, password } = this.state;
+      const { classes } = this.props;
+      
+      return (
+        <div>
+        {errorMessage}
+        {
+          this.props.loading ?
+          
+          <div className={classes.root}>
+            <CircularProgress />
+          </div>
+
+          :
+          
+          <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+              <Avatar className={classes.avatar} style={{margin: 'auto'}}>
+              <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+              Sign in
+              </Typography>
+              <form className={classes.form} noValidate onSubmit={this.handleLogin} method="POST">
+              <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  value={username}
+                  onChange={this.handleChange}
+                  autoFocus
+              />
+              <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value= {password}
+                  onChange={this.handleChange}
+              />
+              <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+              />
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+              >
+                  Sign In
+              </Button>
+              {/* <Grid container>
+                  <Grid item xs>
+                  <Link href="#" variant="body2">
+                      Forgot password?
+                  </Link>
+                  </Grid>
+                  <Grid item>
+                  <Link href="#" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                  </Link>
+                  </Grid>
+              </Grid> */}
+              </form>
+          </div>
+          <Box mt={8}>
+              <Copyright />
+          </Box>
+          </Container>
+
+          }
+        </div>
+      );
+  }
 }
 
 const mapStateToProps = (state) => {
