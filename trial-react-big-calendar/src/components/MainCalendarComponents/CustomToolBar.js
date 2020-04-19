62fee48a9e24e08867d4f855046400d4e468d712
Okseeyou <celiaongsl@gmail.com>
2020-04-19 20:26:12 +0800

    change look of customtoolbar in main calendar

--- trial-react-big-calendar/src/components/MainCalendarComponents/CustomToolBar.js
+++ trial-react-big-calendar/src/components/MainCalendarComponents/CustomToolBar.js
@@ -4,8 +4,6 @@ import { Button, Typography, IconButton } from '@material-ui/core';
 import ArrowBackIos from '@material-ui/icons/ArrowBackIos'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { withStyles } from '@material-ui/core/styles';
-import cn from 'classnames';
-import clsx from 'clsx';
 // import './CustomToolBar.css';
 
 export let navigate = {
@@ -42,60 +40,54 @@ const useStyles = (theme) => ({
 })
 
 class CustomToolBar extends React.Component {
-    view = view => {
-      this.props.onView(view);
-    };
+  view = view => {
+    this.props.onView(view);
+  };
 
-    viewNamesGroup(messages) {
-      let viewNames = this.props.views;
-      const view = this.props.view;
-      const { classes } = this.props;
-  
-      if (viewNames.length > 1) {
-        return viewNames.map(name => (
-          <Button
-            key={name}
-            // className={cn(classes.viewStyling, {
-            //   // active: view === name,
-            //   // 'btn-primary': view === name,
-            // })}
-            // className={classes.viewStyling}
-            className={view === name ? 'rbc-active' : ''}
-            onClick={this.view.bind(null, name)}
-            style={{
-              fontFamily: 'Roboto',
-              // marginLeft: '5px',
-              // marginRight: '15px',
-              // border: '0px solid',
-            }}
-          >
-            {messages[name]}
-          </Button>
-        ));
-      }
+  viewNamesGroup(messages) {
+    let viewNames = this.props.views;
+    const view = this.props.view;
+    const { classes } = this.props;
+
+    if (viewNames.length > 1) {
+      return viewNames.map(name => (
+        <Button
+          key={name}
+          className={view === name ? 'rbc-active' : ''}
+          onClick={this.view.bind(null, name)}
+          style={{
+            fontFamily: 'Roboto',
+          }}
+        >
+          {messages[name]}
+        </Button>
+      ));
     }
+  }
 
   render() {
     let { localizer: { messages }, label, classes } = this.props
-    return(
-        <div className="rbc-toolbar" style={{paddingTop: '15px', paddingBottom: '5px',}}>
-            <span className="rbc-btn-group">
-                <Button size="small" component={ArrowBackIos} onClick={this.navigate.bind(null, navigate.PREVIOUS)}></Button>
-                <Typography className={classes.label}>{label}</Typography>
-                <Button size="small" component={ArrowForwardIosIcon} onClick={this.navigate.bind(null, navigate.NEXT)}></Button>
-            </span>
-            <span className="rbc-toolbar-label"></span>
-            <span className="rbc-btn-group" style={{marginRight: '20px'}}> 
-            {/* This will have to potentially be switching view b/w month and week */}
-              {this.viewNamesGroup(messages)}
-                {/* <button type="button" onClick={this.navigate.bind(null, navigate.MONTH)}>Month</button>
-                <button type="button" onClick={this.navigate.bind(null, navigate.AGENDA)}>AGENDA</button> */}
-            </span>
-        </div>
+    return (
+      <div className="rbc-toolbar" style={{ paddingTop: '15px', paddingBottom: '5px', }}>
+        <span className="rbc-btn-group">
+        <IconButton aria-label="navigate right" component="span" onClick={this.navigate.bind(null, navigate.PREVIOUS)}>
+            <ArrowBackIos fontSize="small"/>
+          </IconButton>
+          <Typography className={classes.label}>{label}</Typography>
+          <IconButton aria-label="navigate right" component="span" onClick={this.navigate.bind(null, navigate.NEXT)}>
+            <ArrowForwardIosIcon fontSize="small"/>
+          </IconButton>
+        </span>
+        <span className="rbc-toolbar-label"></span>
+        <span className="rbc-btn-group" style={{ marginRight: '20px' }}>
+          {/* This will have to potentially be switching view b/w month and week */}
+          {this.viewNamesGroup(messages)}
+        </span>
+      </div>
     )
   }
   navigate = action => {
-      this.props.onNavigate(action)
+    this.props.onNavigate(action)
   }
 }
 
