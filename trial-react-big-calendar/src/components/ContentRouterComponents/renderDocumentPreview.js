1b557613d26fec1c316ce7eaa36fcf757a4c9dbd
Okseeyou <celiaongsl@gmail.com>
2020-05-06 10:14:21 +0800

    prevented overlap of document names inside other submissions view

--- trial-react-big-calendar/src/components/ContentRouterComponents/renderDocumentPreview.js
+++ trial-react-big-calendar/src/components/ContentRouterComponents/renderDocumentPreview.js
@@ -47,7 +47,7 @@ class RenderDocumentPreview extends Component {
         return (
             <div>
                 <Button onClick={this.handleClickOpen}>
-                    <AttachFileIcon style={{ float: 'left' }} />&nbsp;<span >{documentName}</span>
+                    <AttachFileIcon style={{ float: 'left' }} />&nbsp;<span style={{wordBreak: 'break-word'}} >{documentName}</span>
                 </Button>
                 <Dialog
                     open={openDialog}
